ca793576925d76e7d4352ddbdc1d4af50b3c6707
petrstverka-star <petr.stverka@shopsys.com>
2026-01-03 11:53:10 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kondice-v3';
+const CACHE_NAME = 'kondice-v4';
 const ASSETS = [
     './',
     'index.html',
@@ -7,13 +7,32 @@ const ASSETS = [
     'icon-512.png'
 ];
 
+// Install event - caching assets
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => cache.addAll(ASSETS))
     );
 });
 
+// Activate event - cleaning up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Service Worker: Clearing old cache', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Fetch event - serving from cache or network
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
